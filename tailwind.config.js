edcf6892c7d5c54559c979200e5985c931abe906
UncannyPotato69 <get.it.together.beetch@gmail.com>
2024-07-12 20:10:57 +0530

    please work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "pacifico": ["Pacifico"],
+        "heading": ["Playfair Display"],
+        "paragraph": ["Nunito Sans"]
+      },
+      colors: {
+        text: "hsla(var(--text))",
+        bg: "hsla(var(--bg))",
+        secondary_bg: "hsla(var(--secondary_bg))",
+        accent: "hsla(var(--accent))",
+      }
+    },
   },
   plugins: [],
 }
